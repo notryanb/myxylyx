55d124f1de57bdbdcfbd4175cfa2733fcf7c6efb
notryanb <notryanb@gmail.com>
2015-05-13 14:09:45 -0400

    Redfine controls so the player may show and move at the same time. Sample bullets now fire at correct rate.

--- js/play.js
+++ js/play.js
@@ -120,19 +120,10 @@ var playState = {
     // Connect sounds to actions
 
 
-    // Capture keys so the browser doesn't use them for navigation.
-    game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, 
-                                       Phaser.Keyboard.DOWN,
-                                       Phaser.Keyboard.LEFT,
-                                       Phaser.Keyboard.RIGHT]);
+    // Define Key input
+    this.cursors = game.input.keyboard.createCursorKeys();
+    this.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
 
-    // Add support for WASD
-    this.wasd = {
-      up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-      left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-      right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-      shoot: game.input.keyboard.addKey(Phaser.Keyboard.Z)
-    }
 
   },
 
@@ -143,19 +134,27 @@ var playState = {
 
   movePlayer: function(){
   // Key events go here
-    if (this.cursor.left.isDown) {
-      this.player.body.velocity.x = -200;
-    } else if (this.cursor.right.isDown) {
-      this.player.body.velocity.x = 200;
-    } else if (this.cursor.down.isDown) {
-      this.player.body.velocity.y = 200;
-    } else if (this.cursor.up.isDown) {
-      this.player.body.velocity.y = -200;
-    } else if (this.wasd.shoot.isDown) {
-      this.singleBullet.fire(this.player);
-    } else { 
-      this.player.body.velocity.x = 0;
-      this.player.body.velocity.y = 0; }
+
+        this.speed = 300;
+        this.player.body.velocity.set(0);
+
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -this.speed;
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = this.speed;
+        }
+
+        if (this.cursors.up.isDown) {
+            this.player.body.velocity.y = -this.speed;
+        }
+        else if (this.cursors.down.isDown) {
+            this.player.body.velocity.y = this.speed;
+        }
+
+        if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            this.singleBullet.fire(this.player);
+        }
   },
 
   scrollingBackground: function(){
