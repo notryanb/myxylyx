ef3a4010addcc3d72f6a5d741f11dc9b0447aef9
notryanb <notryanb@gmail.com>
2015-05-28 21:54:47 -0400

    Start conversion for making the game an object

--- js/boot.js
+++ js/boot.js
@@ -1,13 +1,49 @@
-var bootState = {
-  preload: function(){
-    game.load.image('progressBar', 'assets/progressBar.png');
+var GameConstants = {
+  // Game constants will go in here for easy changing.
+};
+
+GameConstants.Boot = function (game) {};
+
+GameConstants.Boot.prototype = {
+
+  init: function() {
+
+    // For supporting multi-touch.
+    this.input.maxPointers = 1;
+
+    //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
+    // this.stage.disableVisibilityChange = true;
+
+    if (this.game.device.desktop) {
+      // desktop settings go here
+    } else {
+      //  Same goes for mobile settings.
+      //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.setMinMax(480, 260, 1024, 768);
+      this.scale.forceLandscape = true;
+    }
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
   },
 
-  create: function(){
-    game.stage.backgroundColor = "#8FBC8F";
-    game.renderer.renderSession.roundPixels = true;
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+  preload: function () {
 
-    game.state.start('load');
+    //  Here we load the assets required for our preloader (in this case a loading bar)
+
+    // this.load.image('preloaderBar', 'assets/preloader-bar.png');
+    // this.load.image('bullet', 'assets/bullet.png');
+    // this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
+    // this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+    // this.load.spritesheet('player', 'assets/player.png', 64, 64);
+  },
+
+  create: function () {
+
+    //  By this point the preloader assets have loaded to the cache, we've set the game settings
+    //  So now let's start the real preloader going
+    this.state.start('Preloader');
+
   }
+
 };
